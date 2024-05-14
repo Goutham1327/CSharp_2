4184333768189d9e38950789cf4c2d433dc4ce37
Goutham1327 <bunnyjonpelli@gmail.com>
2024-05-15 00:09:01 +0530

    Create session1.cs

---
+++ session1.cs
@@ -0,0 +1,15 @@
+ 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 25;
+        
+        if (age >= 18)
+        {
+            Console.WriteLine("You are an adult.");
+        }
+    }
+}
